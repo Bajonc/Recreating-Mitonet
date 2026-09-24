b67b7c0133c80aea26294a77bdf4a6f18b8a1c7c
Bartosz Zając <194638935+Bajonc@users.noreply.github.com>
2026-09-24 23:20:49 +0200

    Update __init__.py

--- empanada/models/encoders/__init__.py
+++ empanada/models/encoders/__init__.py
@@ -1,3 +1,5 @@
 from empanada.models.encoders.regnet import *
 from empanada.models.encoders.resnet import *
 from empanada.models.encoders.convnext import *
+from empanada.models.encoders.swin import *
+from empanada.models.encoders.swinv2 import *
